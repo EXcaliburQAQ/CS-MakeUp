93182001ebb9e28bd4fbf106842605becd8f3eeb
pc-dell-liu <2904500451@qq.com>
2019-01-04 17:03:58 +0800

    update lesson 1-11

---
+++ sicp.py
@@ -0,0 +1,19 @@
+def gcd(m,n):
+    """Return the K largest
+
+    k , m, n are all pso
+
+    >>> gcd(12,8)
+    4
+    >>> gcd(16,12)
+    4
+    """
+    pass
+    '''
+    if m == n:
+        return m
+    elif m < n:
+        return gcd(n,m)
+    else:
+        return gcd(m-n,n)
+    '''
